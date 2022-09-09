9fe95d4c540fc1ff193dfaa0df3985a88e8612d5
Issah Nenkenne Safawu <nenkenne360@gmail.com>
2022-09-08 21:23:30 -0700

    modified same file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
- * Prints a string using the puts function
+ * main - Entry point
+ *
+ * Return: always 0
 */
 int main(void)
 {
-	puts("\"programming is like building a multilngual puzzle");
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }

3204210603e1588b5cc56f789fc07768af26e031
Nenkenne Safawu <nenkenn360@gmail.com>
2022-09-13 16:13:18 +0000

    modified 1-alphabet.c file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * print_alphabet - prints alphabet
  *
- * Return: Always 0.
- *
+ * Return: always 0.
+ * 
  */
 void print_alphabet(void)
 {
-	char i;
-
-	for (i = 'a'; i <= 'z'; i++)
-	{	
-		_putchar(i);
+	for (i = 'a'; i <= 'z' i++)
+	{
+		_putchar(i)
 	}
 	_putchar('\n');
 }

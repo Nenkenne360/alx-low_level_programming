2a456d3c9ac081c68a77500720b4ad5802f343b6
Nenkenne360 <111001012+Nenkenne360@users.noreply.github.com>
2022-09-13 19:11:24 +0000

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,24 +1,21 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * print alphabet x10: prints lowercase 10 times
+ * print_alphabet_x10 - prints alphabet 10 times
  *
- * Return: Always 0.
+ * Return: always 0;
  *
  */
 void print_alphabet_x10(void)
 {
 	char i;
-	
-	for (i = 'a'; i <= 'z'; i++)
+	char h;
+
+	for (h = 0; h <= 9; h++)
 	{
+		for (i = 'a'; i <= 'z'; i++)
 		{
-			for (i = 0; i < 10; i++)
-			{
-				_putchar(i);
-			}
+			_putchar(i);
 		}
+		_putchar('\n');
 	}
-		_putchar ('\n');
-		return(0);
 }

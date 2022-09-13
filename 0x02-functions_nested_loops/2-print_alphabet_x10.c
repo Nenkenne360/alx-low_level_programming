ab660eb510e8043454729a28abbb39dfd850c62b
Nenkenne Safawu <nenkenn360@gmail.com>
2022-09-13 19:04:37 +0000

    updated 2 _alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,7 +8,6 @@
 void print_alphabet_x10(void)
 {
 	char i;
-	char h;
 
 	for (h = 0; h <= 9; h++)
 	{
@@ -17,5 +16,6 @@ void print_alphabet_x10(void)
 			_putchar(i);
 		}
 		_putchar('\n');
+		return(0);
 	}
 }

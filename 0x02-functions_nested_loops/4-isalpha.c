31476ce73dd424bd68cc829796cb19e8d0335bfd
Nenkenne360 <111001012+Nenkenne360@users.noreply.github.com>
2022-09-13 23:59:06 +0000

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,16 @@
-#include "main.h"
+#include "holberton.h"
 /**
- * _isalpha - check alphabetic charactern
+ * _isalpha - main function
  *
  * @c: character to compare
  *
  * Return: 1 if true, 0 if false
  *
  */
+
 int _isalpha(int c)
 {
-	for ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
 		return (1);
 	}

b2a56c11f4577718fcf1113f854d513f46933a7a
Nenkenne Safawu <nenkenn360@gmail.com>
2022-09-19 15:12:07 +0000

    added new file

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *swap_int - swaping values of pointers
+ *
+ * @a: pointer
+ *
+ * @b: pionter
+ */
+void swap_int(int *a, int *b)
+{
+	*a = c;
+	*b = d;
+
+	*a = d;
+	*b = c;
+}

3bf9075aaf6948baa6e1c55f3ca85ad031d0a737
Nenkenne360 <111001012+Nenkenne360@users.noreply.github.com>
2022-10-13 23:23:19 +0000

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,28 +1,25 @@
-#ifndef _VARIADIC_FUNCTIONS_H_
-#define _VARIADIC_FUNCTIONS_H_
-
-#include <stdio.h>
+#ifndef _HEADER_
+#define _HEADER_
 #include <stdarg.h>
 
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_a_char(char *separator, va_list args);
+void print_a_integer(char *separator, va_list args);
+void print_a_float(char *separator, va_list args);
+void print_a_char_ptr(char *separator, va_list args);
+
 /**
- * struct p - checker struct
- * @p: possibility
- * @f: Function to be used
+ * struct format_types - Struct format_types
+ * @identifier: The conversion specifier
+ * @f: The function pointer
  */
-typedef struct p
+typedef struct format_types
 {
-  char *p;
-  void (*f)(va_list);
-} checker;
-
-void print_c(va_list);
-void print_i(va_list);
-void print_f(va_list);
-void print_s(va_list);
-
+	char *identifier;
+	void (*f)(char *separator, va_list args);
+} f_dt;
 #endif
-
